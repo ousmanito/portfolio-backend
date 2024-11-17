6dc1e2896ef836efb687b52ef6161776bf4ada84
Ousmane <ousmane.209@gmail.com>
2024-11-17 13:15:57 +0100

    fix: Update FRONTEND_INTERNAL_HOST port to 8080

--- _app/portfolio/settings.py
+++ _app/portfolio/settings.py
@@ -51,7 +51,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3100",
     "http://127.0.0.1:3100",
     "http://0.0.0.0:3100",
-    f"http://{os.getenv('FRONTEND_INTERNAL_HOST')}",
+    f"http://{os.getenv('FRONTEND_INTERNAL_HOST')}:8080",
     f"https://{os.getenv('FRONTEND_EXTERNAL_HOST')}"
 ]
 
