cc9bde909e16611dfa3d8143b0da82a022c4e77d
Ousmane <ousmane.209@gmail.com>
2024-11-17 17:12:19 +0100

    style: Fix inconsistent quotes in settings.py

--- _app/portfolio/settings.py
+++ _app/portfolio/settings.py
@@ -54,7 +54,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://0.0.0.0:3000",
     f"http://{os.getenv('FRONTEND_INTERNAL_HOST')}:8080",
     f"https://{os.getenv('FRONTEND_EXTERNAL_HOST')}",
-    f"https://{os.getenv("DJANGO_ADMIN_EXTERNAL_HOST")}",
+    f"https://{os.getenv('DJANGO_ADMIN_EXTERNAL_HOST')}",
 ]
 
 # Application definition
